acd50cd983d530d3b1c1defb13079c5c48ff76f7
wohlfea <wohlfea@gmail.com>
2016-03-03 13:33:26 -0800

    edited gitignore

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='mail_room',
+    description='A Python program to manage donation thank you emails',
+    version=0.1,
+    author='AJ Wohlfert and Kyle Richardson',
+    license='MIT',
+    py_modules=[],
+    packages_dir={'': 'src'},
+)
